baea63853987dac3ef8b988382726b243049df59
Jonathon Lopez <jonathonlopez@Jonathons-MacBook-Pro.local>
2019-04-08 17:48:50 -0500

    server change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('Hey!')
+  res.send('Hey JLO!')
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'))
