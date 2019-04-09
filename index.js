f59da12a1efc45574449b6fee33dfdebc2bc5e69
Jonathon Lopez <jonathonlopez@Jonathons-MacBook-Pro.local>
2019-04-08 19:10:48 -0500

    hey matt

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('Hey JLO!')
+  res.send('Hey matt!')
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'))
